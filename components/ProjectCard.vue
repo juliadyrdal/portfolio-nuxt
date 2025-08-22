9a7ce9c621f1226f7a36e7de53d0a6cdfcd775f4
juliadyrdal <juliadyrdal@protonmail.com>
2025-08-22 23:58:03 +0100

    Tidy up comments

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -28,7 +28,6 @@ const heroIcon = icons[props.project.icon]
         <div class="hidden lg:flex m-6 min-w-56 min-h-60 bg-white border-2 border-[#333] md:w-1/3 items-center justify-center">
             <component :is="heroIcon" class="w-10 h-10 text-[#333]" />
         </div>
-        <!--End Image container-->
 
         <!--Text content container-->
         <div class="p-6">
@@ -37,7 +36,6 @@ const heroIcon = icons[props.project.icon]
             <div id="headingContainer" class="absolute -top-8 left-1/2 transform -translate-x-1/2 p-3 border-2 border-[#333] bg-white text-center w-4/5 md:w-auto">
                 <h2 class="text-xl md:text-2xl text-[#333] font-bold font-rundDisplay">{{ project.title }}</h2>
             </div>
-            <!--End Heading container-->
 
             <!--Links container-->
             <div>
@@ -56,25 +54,20 @@ const heroIcon = icons[props.project.icon]
                     </li>
                 </ul>
             </div>
-            <!--Links container-->
 
             <!--Tech stack-->
             <div v-if="project.tech" class="flex flex-wrap gap-2 mt-4 uppercase">
                 <p v-for="tech in project.tech" :key="tech" class="text-sm md:text-base">{{ tech }}<span class="ml-2 md:ml-4">|</span></p>
             </div>
-            <!--End Tech stack-->
 
             <!--Text content-->
             <p class="mt-4 mb-2 text-sm md:text-base">
                 {{ project.description }}
                 <NuxtLink :to="project.slug" class="pl-2 font-semibold hover:underline underline-offset-4">Read more...</NuxtLink>
             </p>
-            <!--End Text content-->
 
         </div>
-        <!--End Text content container-->
 
     </div>
-    <!--End Project card-->
 </template>
 
